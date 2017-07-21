10155960b11142fa67916e0aac032a35f23c3ceb
tydunn <tydunn@umich.edu>
2017-07-21 09:19:01 -0400

    Super Reduced String

---
+++ Algorithms/String/reduced-string.cpp
@@ -0,0 +1,40 @@
+// Super Reduced String
+// Day #73
+// Friday, July 21, 2017
+// https://www.hackerrank.com/challenges/reduced-string
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    
+    string og;
+    cin >> og;
+    
+    for (int i = 1; i < og.length(); i++) {
+        
+        if (og[i-1] == og[i]) {
+            
+            og.erase(i-1, 2);
+            i = 0;
+            
+        } // if
+        
+    } // for
+    
+    if (og.empty()) {
+    
+        cout << "Empty String" << endl;
+        
+    } else {
+        
+        cout << og << endl;
+        
+    } // if / else
+    
+    return 0;
+    
+} // main()
