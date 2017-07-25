211e1218ef300f5fbd8f1023812afbc00d0894fa
tydunn <tydunn@umich.edu>
2017-07-24 21:25:45 -0400

    Caesar Cipher

---
+++ Algorithms/String/caesar-cipher-1.cpp
@@ -0,0 +1,36 @@
+// Caesar Cipher
+// Day #75
+// Monday, July 24, 2017
+// https://www.hackerrank.com/challenges/caesar-cipher-1
+
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    int length;
+    string cipher;
+    int rotate;
+    
+    cin >> length >> cipher >> rotate;
+    
+    for (auto &letter : cipher) {
+        
+        if (isalpha(letter)) {
+            
+            char figure = isupper(letter) ? 'A' : 'a';
+            letter = figure + (letter - figure + rotate) % 26;
+            
+        } // if
+        
+    } // for
+    
+    cout << cipher << endl;
+    
+    return 0;
+
+} // main()
