cd9526948e54ea546d786b051764316ff14cd347
tydunn <tydunn@umich.edu>
2017-09-05 08:29:46 -0400

    Permuting Two Arrays

---
+++ Algorithms/Greedy/two-arrays.cpp
@@ -0,0 +1,59 @@
+// Permuting Two Arrays
+// Day #102
+// Monday, September 5, 2017
+// https://www.hackerrank.com/challenges/two-arrays
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    
+    int num;
+    cin >> num;
+    
+    for (int i = 0; i < num; i++) {
+        
+        bool valid = true;
+        
+        int size, thld;
+        cin >> size >> thld;
+        
+        vector<int> arrA(size);
+        vector<int> arrB(size);
+        
+        for (int j = 0; j < size; j++) {
+            
+            cin >> arrA[j];
+            
+        } // for j
+        
+        for (int k = 0; k < size; k++) {
+            
+            cin >> arrB[k];
+            
+        } // for k
+        
+        sort(arrA.begin(), arrA.end());
+        sort(arrB.begin(), arrB.end(), greater<int>());
+        
+        for (int l = 0; l < size; l++) {
+            
+            if (arrA[l] + arrB[l] < thld) {
+                
+                valid = false;
+                
+            } // if
+            
+        } // for l
+        
+        (valid) ? (cout << "YES" << endl) : (cout << "NO" << endl);
+        
+    } // for
+    
+    return 0;
+    
+} // main()
