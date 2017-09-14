2e822f7ceb75a65e96cebed6cc308275db8bcc74
tydunn <tydunn@umich.edu>
2017-09-14 06:57:32 -0400

    Tree: Height of a Binary Tree

---
+++ Data Structures/Trees/tree-height-of-a-binary-tree.cpp
@@ -0,0 +1,42 @@
+// Tree: Height of a Binary Tree
+// Day #111
+// Thursday, September 14, 2017
+// https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree
+
+#include <iostream>
+#include <cstddef>
+
+using namespace std;	
+
+class Node{
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            }
+            else {
+                Node* cur;
+                if(data <= root->data){
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                }
+                else{
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
